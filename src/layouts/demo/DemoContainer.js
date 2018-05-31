e683f00853ae3c487f5effaff4ee1d512c77e2cc
Val <dgm97@cornell.edu>
2018-05-30 21:21:55 -0700

    gitignore and todo comment

--- src/layouts/demo/DemoContainer.js
+++ src/layouts/demo/DemoContainer.js
@@ -5,10 +5,10 @@ import Demo from './Demo.js';
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
+// todo: pass data to smart contract
+
 export default class DemoContainer extends Component {
-  componentDidMount() {
-    
-  }
+
   render() {
     return(
       <Demo/>
